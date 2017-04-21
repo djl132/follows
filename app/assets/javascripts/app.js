2183606f177b79bd524096fb3cd241a50b171b46
DerekLin <djl132@case.edu>
2017-04-20 21:53:27 -0700

    got CDN for angularuirouter I believe, now just got to load the template, but it's still not seeing it, even though, angular ui router seems to be working.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,7 +1,49 @@
 
-angular.module('flapperNews', [])
-.controller('MainCtrl', [
-'$scope',
-function($scope){
-  $scope.test = 'Hello world!';
-}]);
+(function(){
+
+
+  function config($locationProvider, $stateProvider) {
+
+  //PROVIDES RULES ABOUT HOW TO DISPLAY LOCATION url info?
+  $locationProvider
+    .html5Mode({
+      enabled:true,//prevent hashbang - humanly readable form easier to parse path info.
+      requireBase: false
+  });
+
+  //configures EACH component(MVCs) OF THE APP based on states
+  //$stateProvider.state(stateName, stateConfig)
+  //stateName is a unique string that identifies a state and stateConfig is an object that defines specific properties of the state.
+  $stateProvider //METHOD-CHAINING
+    .state('home', {
+        url:'/',
+        templateUrl: '/home/_home.html',
+        controller:'HomeCtrl as home'
+        // resolve: {
+        //   // get real-time questions from backend
+        //   questionsFeed: ['questions', function(questions){
+        //     return questions.getAll();
+        //   }]
+        // }
+  })
+
+    // .state('questions', {
+    //     url:'/questions/{id}',
+    //     templateUrl: 'questions/_question.html',
+    //     controller: 'QuestionsCtrl as question',
+    //     resolve: {
+    //       question: ['$stateParams', 'questions', function($stateParams, questions) {
+    //         return questions.get($stateParams.id);
+    //       }]
+    //     }//SIMPLY TELLS VIEW OF STATE WHICH CONTROLLER TO USE, DOES NOT GIVE IT ACCESS
+    // })
+
+  // $urlRouterProvider.otherwise('home');
+
+    };
+
+
+  angular.module('dunno', ['templates','ui.router'])
+  .config(config)
+
+})();
---
+++ app/assets/javascripts/home/homeCtrl.js
@@ -0,0 +1,32 @@
+
+
+// DATA IS RETRIEVED AND CHNAGED THROUGH MODEL DATA AND METHODS, CONTAINED IN SERVICES
+(function(){
+
+function HomeCtrl($scope){
+  // BIND question MODEL DATA TO FORNT END, thorugh a SERVICE
+  // $scope.questions = questions.questions
+  //
+  // $scope.ask = function(){
+  //   if(!$scope.title || $scope.title === '' || $scope.body || $scope.body === '') { return; }
+  //
+  //   // CREATS question ON BACKEND ANDTHEN UPDATES
+  //   //IT IN THE FRONTEND
+  //   questions.create({
+  //     title: $scope.title,
+  //     body: $scope.body,
+  //     upvotes: 0
+  //   });
+  //   $scope.title = '';
+  //   $scope.body = '';
+  // };
+
+  }
+
+
+angular
+.module('dunno')
+.controller('HomeCtrl', ['$scope','templates', HomeCtrl])
+
+
+})();
